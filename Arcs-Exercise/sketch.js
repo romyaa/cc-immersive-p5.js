88ea619f6f711774bb94eaca7ba4a98ca81400a5
RomyAudrey <romyaboudarham@gmail.com>
2022-09-14 14:56:26 -0700

    made the eye, nose, and mouth of my self portrait

--- Arcs-Exercise/sketch.js
+++ Arcs-Exercise/sketch.js
@@ -1,17 +1,18 @@
 function setup() {
-  createCanvas(500,500);
-  angleMode(DEGREES);
+  createCanvas(500,220);
 }
 
 function draw() {
   background(240);
-  // arc(90,60,80,80,0,HALF_PI);
-  // arc(190,60,80,80,0,PI+HALF_PI);
-  // arc(290,60,80,80,PI,HALF_PI);
-  // arc(390,60,80,80,QUARTER_PI,PI+QUARTER_PI);
+  angleMode(RADIANS);
+  arc(90,60,80,80,0,HALF_PI);
+  arc(190,60,80,80,0,PI+HALF_PI);
+  arc(290,60,80,80,PI,HALF_PI);
+  arc(390,60,80,80,QUARTER_PI,PI+QUARTER_PI);
 
-  arc(90,60,80,80,0,90);
-  arc(190,60,80,80,0,270);
-  arc(290,60,80,80,180,90);
-  arc(390,60,80,80,45,225);
+  angleMode(DEGREES);
+  arc(90,160,80,80,0,90);
+  arc(190,160,80,80,0,270);
+  arc(290,160,80,80,180,90);
+  arc(390,160,80,80,45,225);
 }
--- Self-portrait/sketch.js
+++ Self-portrait/sketch.js
@@ -2,24 +2,31 @@ var gif_loadImg, gif_createImg;
 
 function preload() {
   disco_ball = loadImage("disco-ball.gif");
+  dancer1 = loadImage("dancer1.gif");
+  dancer2 = loadImage("dancer2.gif");
+  checker = loadImage("checker.png");
 }
 
 function setup(){
-    createCanvas(700,700);
+    createCanvas(700,700,WEBGL);
 }
 
-eyeX = 400;
-eyeY = 200;
+var eyeX = 400;
+var eyeY = 200;
+var noseLightX = eyeX-20;
+var noseLightY = eyeY+20;
 
 function draw() {
-    background(255);
-    // gif_createImg = createImg("disco-ball.gif");
-    // gif_createImg.position(350, 350);
-    disco_ball.resize(53,53)
+    background(30);
+    translate(-width/2,-height/2,0); //moves our drawing origin to the top left corner
+    disco_ball.resize(57,57);
+    dancer1.resize(70,70);
+    dancer2.resize(60,70);
 
     /* right eye start */
-
-    fill(255,255,0);
+    stroke(0);
+    strokeWeight(2);
+    fill(245,218,223);
     beginShape();
     vertex(eyeX,eyeY);
     vertex(eyeX+40,eyeY-40);
@@ -27,6 +34,30 @@ function draw() {
     vertex(eyeX+40,eyeY+30);
     endShape(CLOSE);
 
-    image(disco_ball, eyeX+15,eyeY-29);
+    image(disco_ball, eyeX+14,eyeY-33);
     /* right eye end */
+    stroke(0,255,0)
+    line(eyeX+40,eyeY-40,eyeX-130, eyeY+130);
+    stroke(255,16,240);
+    line(eyeX-130, eyeY+130, noseLightX,eyeY+250);
+    stroke(255,95,31);
+    line(noseLightX,eyeY+250, eyeX+65, eyeY+180);
+    stroke(255, 240, 31);
+    line(eyeX+65, eyeY+180,eyeX+100,eyeY);
+
+    /* spot light start */
+    strokeWeight();
+    fill(255,255,220);
+    triangle(noseLightX, noseLightY, noseLightX-50, noseLightY+150, noseLightX+50, noseLightY+150);
+    fill(255,255,220,255);
+    ellipse(noseLightX,noseLightY+150,100,50);
+    
+    texture(checker);
+    ellipse(noseLightX,noseLightY+150,100,50);
+    fill(255,255,220,90);
+    ellipse(noseLightX,noseLightY+150,100,50);
+    image(dancer1, noseLightX-10,noseLightY+90);
+    image(dancer2, noseLightX-50,noseLightY+100);
+
+    /* spot light end */
 }
